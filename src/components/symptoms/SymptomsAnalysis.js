f21f8c717401060fa57edc1a55160c970f58376c
Nick Chubb <Nick.r.chubb@gmail.com>
2020-04-20 20:14:02 -0700

    More work on algorithm

--- src/components/symptoms/SymptomsAnalysis.js
+++ src/components/symptoms/SymptomsAnalysis.js
@@ -53,19 +53,81 @@ const POPULATION_WITH_FLU_PER_DAY = POPULATION * PERCENT_POPULATION_WITH_FLU_PER
 // BC = ~6%
 const PERCENTAGE_CHANCE_COVID = COVID_CASES[province] / POPULATION_WITH_FLU_PER_DAY;
 
+
+/* 
+    The rest of the algorithm uses this base rate to calculate how much more likely your symptoms are to be 
+    COVID instead of the Flu
+*/
+
+
 // reflects the percentage of diagnosed COVID cases that
-const FEVER_RATE = .88;
-const DRY_COUGH_RATE = .68;
-const FATIGUE_RATE = .38;
-const WET_COUGH_RATE = .33;
-const SHORTNESS_OF_BREATH_RATE = .19;
-const ACHES_RATE = 15;
-const SORE_THROAT_RATE = .14;
-const HEADACHE_RATE = .14;
-const CHILLS_RATE = .14;
-const NAUSEA_RATE = .05;
-const NASAL_CONGESTION_RATE = .05;
-const DIARRHEA_RATE = .04;
+const COVID_RATES = {
+        'rarely':
+        {
+            'diarrhea': .04,
+            'nasal congestion': .05,
+            'nausea': .05
+
+        },
+        'sometimes': 
+        {   
+            'chills': .14,
+            'headache': .14,
+            'sore throat': .14,
+            'aches': .15
+        },
+        'common': 
+        {
+            'shortness of breath': .19
+
+        },
+        'fairly common': 
+        {
+            'wet cough': .33,
+            'fatigue': .38
+        },
+        'usual': 
+        {
+            'dry cough': .68,
+            'fever': .88
+        }
+};
+
+const FLU_RATES = {
+    'rarely':
+    {
+        'diarrhea': 1,
+        'nausea': 1
+
+    },
+    'sometimes': 
+    {   
+        'nasal congestion': 1,
+        'sore throat': .14
+    },
+    'common': 
+    {
+        'headache': .14,
+        'dry cough': .33,
+        'wet cough': .60
+
+    },
+    'fairly common': 
+    {
+        'chills': .6
+    },
+    'usual': 
+    {
+        'fatigue': .68,
+        'fever': .80,
+        'aches': .80
+    }
+};
+
+const SEVERE_CHEST_PAIN = 0;
+const SEVERE_DIFFICULTY_BREATHING = 0;
+const DIFFICULTY_WAKING_UP = 0;
+const FEELING_CONFUSED = 0;
 
 // how much does it increase your chance if you have severe symptoms?
 
