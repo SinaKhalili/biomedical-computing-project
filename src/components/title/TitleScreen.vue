9ff4a6da5ebdacafa3f82be4a6c164ca97196894
Nick Chubb <Nick.r.chubb@gmail.com>
2020-04-21 04:28:04 -0700

    Fix margins

--- src/components/title/TitleScreen.vue
+++ src/components/title/TitleScreen.vue
@@ -3,16 +3,18 @@
 <template>
     <div class="title">
         <h1>🦠 BioBuddy - the COVID Calculator 😷</h1>
+        <br>
         <h2>Calculate your own personal health risks caused by COVID-19.</h2>
-        <p>
-            <i id="disclaimer">
-                DISCLAIMER: This website is a should be used only for a general assessment of risk.
-                In no way does this website diagnose anyone who uses it with COVID-19. For a more information
-                on how to handle your own personal risk facing COVID-19 please contact your respective center for
-                disease control. 
-            </i>
-        </p>
-        <el-button type="primary" id="start-button" v-on:click="$emit('next')">Start</el-button>
+        <div class="disclaimer">
+        <i>
+            <u>DISCLAIMER:</u> This website is a should be used only for a general assessment of risk.
+            In no way does this website diagnose anyone who uses it with COVID-19. For a more information
+            on how to handle your own personal risk facing COVID-19 please contact your respective center for
+            disease control. 
+        </i>
+        </div>
+        <br><br>
+        <div><el-button type="primary" id="start-button" v-on:click="$emit('next')">Start</el-button></div>
     </div>
 </template>
 
@@ -28,7 +30,9 @@ export default {
 }
 
 .disclaimer {
-    margin-left: 10%;
-    margin-right: 10%;
+    width: 60%;
+    margin-left: 20%;
+    margin-right: 20%;
+    text-align: center;
 }
 </style>
